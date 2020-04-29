e00f7938d01a87528ade9dddaa525c4c26ee1f4b
harishss3 <61965991+harishss3@users.noreply.github.com>
2020-04-29 17:55:44 +0530

    Update cat.py

--- Python/cat.py
+++ Python/cat.py
@@ -1,8 +1,6 @@
 # Import the argparse library
 import argparse
 
-import os
-import sys
 
 # Create the parser
 my_parser = argparse.ArgumentParser(prog='pycat',description='List the content of a folder')
@@ -11,23 +9,39 @@ my_parser = argparse.ArgumentParser(prog='pycat',description='List the content o
 my_parser.add_argument('Name',metavar='name',type=str, help='the names of the file',nargs='+')
 my_parser.add_argument('-n',action='store_true',help='Numbers all the lines')
 my_parser.add_argument('-E',action='store_true',help='print $ at the end of each line')
+my_parser.add_argument('-o',help='name of output file')
 
 # Execute the parse_args() method
 args = my_parser.parse_args()
 
 input_name = args.Name
+output=None
+try:
+	fname = args.o
+	output = open(fname,'w')
+	print("Redirect from Python:",file=output)
+except:
+	output=None
+
 i=1
 for name in input_name:
 	try:
 		fhand = open(name)
-		endwith='\n'
-		if args.E:
-			endwith='$'
-		for line in fhand:
-			line=line.rstrip()
-			if args.n:
-				print(i,end=' ')
-				i=i+1
-			print(line,end=endwith)
 	except:
 		print('!!File',name,'does not exist!!')
+		break
+	endwith='\n'
+	if args.E:
+		endwith='$'
+	for line in fhand:
+		line=line.rstrip()
+		if args.n:
+			print(i,end=' ',file=output)
+			i=i+1
+		print(line,end=endwith,file=output)
+
+'''
+To add:
+1. using cat to read from stdin(i.e. cat without arguments)
+2. -b numbering non blank lines
+'''
